9b87150afedc3f5aecf2f29a4d4e01e3a030fdf9
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2026-02-21 10:50:10 +0100

    add possibilty to seeing content of subfolders

--- backend/app/routers/admin/images.py
+++ backend/app/routers/admin/images.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 
 from app import schemas, security
 
@@ -12,10 +12,15 @@ IMAGES_DIR = os.getenv("IMAGES_DIR")
 IMAGES_PATH = Path(IMAGES_DIR).resolve()
 
 @router.get("", response_model=List[schemas.FolderContent])
-async def read_folder_content():
-    target_path = (IMAGES_PATH).resolve()
+async def read_folder_content(folder_path: str = Query(".", alias="path")):
+    target_path = (IMAGES_PATH / folder_path).resolve()
+    
+    if not str(target_path).startswith(str(IMAGES_PATH)):
+        raise HTTPException(status_code=403, detail="You do not have authority to achive that folder")
+    
     if not target_path.exists():
         raise HTTPException(status_code=404, detail=f"Folder {IMAGES_PATH} does not exist")
+    
     content = []
     for item in target_path.iterdir():
         relative_item_path = str(item.relative_to(IMAGES_PATH))
